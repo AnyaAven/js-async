3793c8b2f3d993bdb7d2364d4723b657815e7c20
Anya Aven <leafeon96@gmail.com>
2024-05-13 19:31:38 -0400

    Finished

--- show-numbers.js
+++ show-numbers.js
@@ -2,11 +2,27 @@
 
 const URL_NUMBERS_API = "http://numbersapi.com";
 
-/** Get json trivia information from Numbers API about num */
+/** Get trivia information from Numbers API about num
+ *
+ * Log out trivia text.
+ *
+ * Trivia info ex for num 42:
+ * {
+    text: '42 is the number of kilometers in a marathon.',
+    number: 42,
+    found: true,
+    type: 'trivia'
+  }
+*/
 async function showNumberTrivia(num) {
-  const resp = await fetch(`URL_NUMBERS_API/${num}`);
-  const numTrivia = resp.json();
+  const resp = await fetch(`${URL_NUMBERS_API}/${num}?json`);
+  const numTrivia = await resp.json();
 
-  console.log(numTrivia)
-  return numTrivia
+  // Log out the trivia fact of the num
+  console.log(numTrivia.text);
+
+  // Return json
+  return numTrivia;
 }
+
+showNumberTrivia(42);
